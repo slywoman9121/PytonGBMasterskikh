b66fb9c95f1fbe226cfb26526cc745c558e5d2ff
MasAlena <sly.woman91@gmail.com>
2021-11-08 02:16:20 +0600

    Выполнение ДЗ урок №1 задание №2

--- Lesson1_2.py
+++ Lesson1_2.py
@@ -5,4 +5,9 @@ if users_time_s > 59:
     user_time_m = int(users_time_s % 3600)
     user_time_m = int(user_time_m // 60)
     users_time_s = int(users_time_s % 60)
-print (f"Hi, {user_name}! Your running time:{user_time_h:02}:{user_time_m:02}:{users_time_s:02}")
+    print (f"Hi, {user_name}! Your running time:{user_time_h:02}:{user_time_m:02}:{users_time_s:02}")
+else :
+    user_time_h=0
+    user_time_m=0
+    print(f"Hi, {user_name}! Your running time:{user_time_h:02}:{user_time_m:02}:{users_time_s:02}")
+
