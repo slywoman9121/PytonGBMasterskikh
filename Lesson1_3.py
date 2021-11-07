5ef8f27e01670038bdc0c3e5ce3b3806802d79c2
MasAlena <sly.woman91@gmail.com>
2021-11-08 01:59:30 +0600

    Выполнение ДЗ урок №1, задание №3

---
+++ Lesson1_3.py
@@ -0,0 +1,6 @@
+user_number = input('Введите число? - ')
+user_numberpp = user_number+user_number
+user_numberppp= user_number+user_numberpp
+print(user_number, user_numberpp, user_numberppp)
+sum=(int(user_number)+int(user_numberpp)+int(user_numberppp))
+print(sum)
