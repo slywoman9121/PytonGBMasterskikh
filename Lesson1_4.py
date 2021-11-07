dd4e783f18797569f954d93165e5d1478da889d5
MasAlena <sly.woman91@gmail.com>
2021-11-08 02:46:41 +0600

    Выполнение ДЗ урок №1 задание №4

--- Lesson1_4.py
+++ Lesson1_4.py
@@ -1,2 +1,10 @@
-user_number = input('Введите целое положительное число? - ')
-print (user_number)
+user_number = int(input('Введите целое положительное число? - '))
+#print (user_number)
+big=0
+while user_number > 0 :
+    prom = user_number % 10
+    user_number = user_number // 10
+    if prom>big:
+        big=prom
+print(big)
+print("Program ended")
