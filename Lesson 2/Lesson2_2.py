4dc4bf8abc17d180980464900fcedce5dc304644
MasAlena <sly.woman91@gmail.com>
2021-11-18 00:21:11 +0600

    Выполнение ДЗ Урок №4 Задание №4

--- Lesson 2/Lesson2_2.py
+++ Lesson 2/Lesson2_2.py
@@ -1,9 +1,9 @@
 user_name = input('Hi, what is your name? - ')
 print(f"Hi, {user_name}")
 str_w = input('Enter the value of the list separated by a space - ').split()
-print (str_w)
-d_str_w = len(str_w)
-print(d_str_w)
+#print (str_w)
+#d_str_w = len(str_w)
+#print(d_str_w)
 indx = 0
 if d_str_w > 1 :
     while indx<d_str_w-1 :
@@ -11,4 +11,4 @@ if d_str_w > 1 :
         str_w[indx]=str_w[indx+1]
         str_w[indx+1]=tt
         indx=indx+2
-print(str_w)
+print (list(str_w))
