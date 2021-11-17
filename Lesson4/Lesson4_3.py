2878b35c4d6933c7eda87c93937d68585b93ff6b
MasAlena <sly.woman91@gmail.com>
2021-11-18 00:02:32 +0600

    Выполение ДЗ Урок №4 Задание №3

---
+++ Lesson4/Lesson4_3.py
@@ -0,0 +1,6 @@
+user_name = input('Hi, what is your name? - ')
+print(f"Hi, {user_name}")
+#str_w = [30, 2, 13, 45, 23, 2, 3, 1, 34]
+#print(str_w)
+znach = [i for i in range(20, 241) if ((i % 21 == 0) or (i % 20 == 0))]
+print(znach)
