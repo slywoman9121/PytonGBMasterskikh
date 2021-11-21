7275aa6b57d23cf967c0111ab84226870d8015f0
MasAlena <sly.woman91@gmail.com>
2021-11-21 22:18:20 +0600

    Выполнение ДЗ Урок №5 Задание №3

--- Lesson 5/Lesson5_3.py
+++ Lesson 5/Lesson5_3.py
@@ -1,6 +1,7 @@
 user_name = input('What is your name? - ')
 print(f"Hi, {user_name}")
 salary = 0
+Minimum = []
 with open("ThirdFile.txt", "r") as str:
     for line in str:
         print(line.strip())
@@ -9,8 +10,10 @@ with open("ThirdFile.txt", "r") as str:
         # ttt = str(rr[0])
         salary += int(rr[1])
         if yy < 20000:
-            print(f"Minimum salary - {rr[0]}")
+            Minimum.append(rr[0])
+            # print(f"Minimum salary - {Minimum}")
             # print(f"Type {type(yy)}")
+    print(f"Minimum salary - {Minimum}")
     with open('ThirdFile.txt') as str:
         pr = sum(1 for i in str)
         szp=int(salary/pr)
